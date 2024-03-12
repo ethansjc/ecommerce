290fae753aa8c90270dbec7d837005d4d694309f
ethansjc <ethansjcheong@gmail.com>
2024-03-11 23:00:55 -0400

    Created starter files for account challenge

---
+++ src/app/(pages)/account/UserInfo/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserInfo = () => {
+  return (
+    <div>My UserInfo</div>
+  )
+}
+
+export default UserInfo
---
+++ src/app/(pages)/account/orders/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div> My Orders</div>
+  )
+}
+
+export default Orders
